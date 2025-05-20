d686040e3c559d9a0dbeb19abeb2ea604c10a966
GeekTale <accounts@botnest.es>
2025-05-21 00:38:01 +0200

    Actualizar favicon con logo de BotNest

--- botnest-blog/src/consts.ts
+++ botnest-blog/src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'BotNestBlog - Tecnología educativa y creatividad';
+export const SITE_DESCRIPTION = 'Explora robótica, IA, impresión 3D y más herramientas tecnológicas para el aula.';
